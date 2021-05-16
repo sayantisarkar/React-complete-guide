b586b39cdda2bfd85e828e85bcd2777e51c8e14c
sayantisarkar <sayanti.sarkar@techcarrot.ae>
2021-05-16 16:06:24 +0400

    State and button functionality

--- src/App.js
+++ src/App.js
@@ -17,7 +17,23 @@ import Person from "./components/Person";
 class App extends Component(){
   //defining state
   state = {
+    persons:[
+      {name:"Ria" , age:"29"},
+      {name:"Jia" , age:"21"},
+      {name:"Tina", age:"27"}
+    ],
+    otherState:'some other value',
+    showPersons: false
+  }
 
+  switchNamehandler = (newName)=>{
+    this.setState({
+      persons:[
+        {name:newName , age:"29"},
+        {name:"Jia" , age:"21"},
+        {name:"Tina", age:"27"}
+      ]
+    })
   }
 
   render () {
@@ -25,9 +41,23 @@ class App extends Component(){
       <div className="App">
       <h1>Hiiiii Pooja</h1>
       <p>Are you there??</p>
-      <Person name="Ria" age="29"/>
+      <button style={style} onClick={()=>this.switchNamehandler('Sayanti')}>Switch Name</button>
+      <div>
+      <Person 
+        name= {this.state.persons[0].name}
+        age= {this.state.persons[0].age} />
+      <Person 
+        name= {this.state.persons[1].name}
+        age= {this.state.persons[1].age} />
+      <Person 
+        name= {this.state.persons[2].name}
+        age= {this.state.persons[2].age} />
+      </div>
+    
+
+      {/* <Person name="Ria" age="29"/>
       <Person name="Jia" age="21">My hobbies are cooking and dancing</Person>
-      <Person name="Tina" age="27"/>
+      <Person name="Tina" age="27"/> */}
      </div>
      );
   }
