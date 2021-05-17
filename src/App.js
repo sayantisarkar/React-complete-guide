ef46db64d139871e0f61993940425275b43c5147
sayantisarkar <sayantisarkar088@gmail.com>
2021-05-17 18:17:52 +0400

    List state and keys

--- src/App.js
+++ src/App.js
@@ -18,22 +18,22 @@ class App extends Component {
   //defining state
   state = {
     persons:[
-      {name:"Ria" , age:"29"},
-      {name:"Jia" , age:"21"},
-      {name:"Tina", age:"27"}
+      {id: "agdg1",name:"Ria" , age:"29"},
+      {id: "schc12",name:"Jia" , age:"21"},
+      {id: "dijoj4",name:"Tina", age:"27"}
     ],
     otherState:'some other value',
     showPersons: false
   }
 
-  switchNamehandler = (newName)=>{
+  switchNamehandler = (event)=>{
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons:[
-        {name:newName , age:"29"},
-        {name:"Jia" , age:"21"},
-        {name:"Tina", age:"27"}
+        {id: "agdg1" , name:event.target , age:"29"},
+        {id: "schc12" ,name:"Jia" , age:"21"},
+        {id: "dijoj4" ,name:"Tina", age:"27"}
       ]
     })
   }
@@ -44,6 +44,16 @@ class App extends Component {
     this.setState({
       showPersons: !doShow
     })
+  }
+
+  deletePersonHandler =(index)=>{
+   //const persons = this.state.persons;
+   //const persons = this.state.persons.slice; // copies array
+   const persons = [...this.state.persons]; //spread operator: ES6
+   persons.splice(index,1);
+   this.setState({
+     persons : persons
+   })
   }
 
   render () {
@@ -60,7 +70,18 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
       <div>
-      <Person 
+
+       {this.state.persons.map((person,index)=>{
+         return <Person click= {()=> this.deletePersonHandler(index)} 
+         name = {person.name} 
+         age={person.age} 
+         key={person.id}
+         changed = {()=>this.switchNamehandler()}
+         />
+       })}
+
+
+      {/* <Person 
         name= {this.state.persons[0].name}
         age= {this.state.persons[0].age} />
       <Person 
@@ -68,7 +89,7 @@ class App extends Component {
         age= {this.state.persons[1].age} />
       <Person 
         name= {this.state.persons[2].name}
-        age= {this.state.persons[2].age} />
+        age= {this.state.persons[2].age} /> */}
       </div>
       );
     }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -4,9 +4,10 @@ import './Person.css';
 const Person = (props)=> {
  //return <p>I am a person and I am {Math.floor(Math.random()*30)} years old</p>;
  return(
- <div>
- <p>I am {props.name} and my age is {props.age}</p> 
+ <div className="Person">
+ <p onClick = {props.click}>I am {props.name} and my age is {props.age}</p> 
  <p>{props.children}</p>
+ <input type= "text" onChange={props.changed} value={props.name} />
  </div>
  );
 }
